965f447d1462804e95d9a0b27d6409da09335f1a
Maks Mushyn <musinmaksim28@gmail.com>
2025-11-04 15:41:50 +0200

    implement UOW pattern

---
+++ Catalog.Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Infrastructure.Repositories.Interfaces;
+
+namespace Catalog.Infrastructure.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        IProductImageRepository ProductImages { get; }
+        IProductDetailRepository ProductDetails { get; }
+        IProductCategoryRepository ProductCategories { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Catalog.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Infrastructure.Persistence;
+using Catalog.Infrastructure.Repositories.Implementations;
+using Catalog.Infrastructure.Repositories.Interfaces;
+
+namespace Catalog.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly CatalogDbContext _context;
+
+        public IProductRepository Products { get; }
+        public ICategoryRepository Categories { get; }
+        public IProductDetailRepository ProductDetails { get; }
+        public IProductImageRepository ProductImages { get; }
+        public IProductCategoryRepository ProductCategories { get; }
+
+        public UnitOfWork(CatalogDbContext context)
+        {
+            _context = context;
+            Products = new ProductRepository(_context);
+            Categories = new CategoryRepository(_context);
+            ProductDetails = new ProductDetailRepository(_context);
+            ProductImages = new ProductImageRepository(_context);
+            ProductCategories = new ProductCategoryRepository(_context);
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
