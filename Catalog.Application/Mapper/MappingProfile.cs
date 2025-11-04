d7c57869abcabfdd579a025e5288d27c7ca364d5
Maks Mushyn <musinmaksim28@gmail.com>
2025-11-05 01:51:23 +0200

    add automapper

---
+++ Catalog.Application/Mapper/MappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Catalog.Application.DTO.Category;
+using Catalog.Application.DTO.Product;
+using Catalog.Application.DTO.ProductCategory;
+using Catalog.Application.DTO.ProductDetail;
+using Catalog.Application.DTO.ProductImage;
+using Catalog.Domain.Enteties;
+
+namespace Catalog.Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Product mappings
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+            CreateMap<Product, ProductDto>();
+
+            // Category mappings
+            CreateMap<CreateCategoryDto, Category>();
+            CreateMap<UpdateCategoryDto, Category>();
+            CreateMap<Category, CategoryDto>();
+
+            // Other mappings (без змін)
+            CreateMap<ProductDetail, ProductDetailDto>().ReverseMap();
+            CreateMap<ProductImage, ProductImageDto>().ReverseMap();
+            CreateMap<ProductCategory, ProductCategoryDto>().ReverseMap();
+        }
+    }
+}
